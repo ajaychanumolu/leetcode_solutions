ff25f2add3d839c3ddb57403a769e67563704f5b
ajaychanumolu <139101630+ajaychanumolu@users.noreply.github.com>
2025-04-01 07:22:13 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 75.35%] - LeetPush

--- Remove-Nth-Node-From-End-of-List.java
+++ Remove-Nth-Node-From-End-of-List.java
@@ -12,20 +12,23 @@ class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         int len = 0;
         ListNode curr = head ;
-
+// finding the lenght of the li ked list 
         while(curr != null){
             len ++;
             curr = curr.next ;
             }
+// if n is equal to len then remove the 1st element and return the list 
         if(n == len){
             return head.next;
         }
         int counter = 0;
         curr = head ;
+// going the before element which we want to remove 
         while(counter < len - n - 1){
             curr = curr.next;
             counter ++;
         }
+// removing the element .
         curr.next = curr.next.next ;
         return head ;
     }
