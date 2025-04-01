bd2f939cc8d1a2a8310c16b34b1a73ccb436f964
ajaychanumolu <139101630+ajaychanumolu@users.noreply.github.com>
2025-04-01 06:45:04 +0530

    [2140] [Time Beats: 96.96%] [Memory Beats: 95.89%] - LeetPush

---
+++ Solving-Questions-With-Brainpower.java
@@ -0,0 +1,24 @@
+
+//(curr_idx+brainpower+1).
+// how to select 1st one ?? 
+
+class Solution {
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n + 1]; 
+        
+        for (int i = n - 1; i >= 0; i--) {
+            int points = questions[i][0];
+            int brainpower = questions[i][1];
+            
+            int nextIndex = i + brainpower + 1;
+            long solve = points + (nextIndex < n ? dp[nextIndex] : 0);
+            
+            long skip = dp[i + 1];
+            
+            dp[i] = Math.max(solve, skip);
+        }
+        
+        return dp[0]; 
+    }
+}
