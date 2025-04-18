8a418c11ea5db98a54f8d0322effcfa569e45c15
ajaychanumolu <139101630+ajaychanumolu@users.noreply.github.com>
2025-04-18 15:22:59 +0530

    [509] [Time Beats: 48.65%] [Memory Beats: 64.08%] - LeetPush

--- Fibonacci-Number.java
+++ Fibonacci-Number.java
@@ -1,14 +1,7 @@
 class Solution {
     public int fib(int n) {
-        if (n == 0) {
-            return 0;
+        if(n<=1) return n;
+        else if (n<5) return n-1;
+        else return fib(n-1) + fib(n-2);
         }
-        if(n == 1 || n == 2)
-        {
-            return 1 ;
-        }
-
-        return fib(n -1) + fib(n-2) ;
-
-    }
 }
