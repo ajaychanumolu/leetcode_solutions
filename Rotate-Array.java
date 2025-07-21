b02ddb214a2692d53d75277fb9db42ff70771083
ajaychanumolu <139101630+ajaychanumolu@users.noreply.github.com>
2025-07-21 19:31:45 +0530

    [189] [Time Beats: 100.00%] [Memory Beats: 13.70%] - LeetPush

--- Rotate-Array.java
+++ Rotate-Array.java
@@ -1,13 +1,18 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k % n;
-        int[] temps = new int[n];
-        for(int i=0 ; i<nums.length ; i++){
-            temps[(i+k)%nums.length] = nums[i];
-        }
-        for(int i= 0;i<n;i++){
-            nums[i] = temps[i];
+        k = k % n ;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+    }
+    private void reverse(int nums[], int start , int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp ;
+            start ++;
+            end -- ;          
         }
     }
 }
