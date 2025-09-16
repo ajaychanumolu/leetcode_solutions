846f25510ed6bdd6d64ce223663c65abd1c8a309
ajaychanumolu <139101630+ajaychanumolu@users.noreply.github.com>
2025-09-16 22:17:12 +0530

    [577] [Time Beats: 37.55%] - LeetPush

---
+++ Employee-Bonus.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT
+    name ,
+    bonus
+FROM 
+    Employee e
+ LEFT JOIN 
+    Bonus b
+ON
+    e.empId = b.empId
+WHERE 
+    bonus < 1000  
+OR
+    b.bonus IS NULL ;
