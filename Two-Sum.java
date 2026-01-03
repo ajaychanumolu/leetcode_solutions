d89637eb2bd258bfe33e1cb403ed47cbfda0e03d
ajaychanumolu <139101630+ajaychanumolu@users.noreply.github.com>
2026-01-03 10:48:28 +0530

    [1] [Time Beats: 28.28%] [Memory Beats: 33.00%] - LeetPush

--- Two-Sum.java
+++ Two-Sum.java
@@ -1,14 +1,15 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j}; 
-                }
-            }
-        }
-        return new int[]{};  
-    }
-}
-
-
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        // we want to use hash set 
+4        for(int i = 0 ; i < nums.length ; i++){
+5            for(int j = i + 1 ; j < nums.length ; j ++){
+6                if(nums[i] + nums[j] == target){
+7                    return new int[]{i, j};
+8                }
+9            }
+10            
+11            
+12        }
+13        return new int[0];
+14    }
+15}
